5ac1e877d6c144f21513f60c54bc63be76fc999c
Felix <felix@malmsjo.com>
2022-03-22 20:05:46 +0100

    Change to serve static content instead of endpoints for each file

--- src/main.cpp
+++ src/main.cpp
@@ -152,16 +152,14 @@ void setup()
     
     // Setting up API endpoints
     Serial.print("Setting up HTTP handlers");
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/index.html", "text/html"); });
-    server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/style.css", "text/stylesheet"); });
-    server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/script.js", "text/javascript"); });
+
+    // Make sure we can access regular files from the server
+    server.serveStatic("/", SPIFFS, "/");
 
     server.on("/api/v1/get_data", HTTP_GET, [](AsyncWebServerRequest *request)
               {
-                  AsyncResponseStream *response = request->beginResponseStream("text/plain");
+                  AsyncResponseStream *response = 
+                    request->beginResponseStream("text/plain");
                   double temps[temperatureBuf.len];
                   temperatureBuf.getBuf(temps);
                   for (int i = 0; i < temperatureBuf.len; i++)
@@ -170,15 +168,18 @@ void setup()
                   }
                   request->send(response);
               });
-    server.on("/api/v1/get_timestep", HTTP_GET, [](AsyncWebServerRequest *request)
+    server.on("/api/v1/get_timestep",
+              HTTP_GET,
+              [](AsyncWebServerRequest *request)
               {
                 request->send(200, "text/plain", String(SAMPLE_TIME));
               });
     server.on("/api/v1/set_temp",
               HTTP_POST,
-              [](AsyncWebServerRequest *request){}, // On request
-              NULL,                                 // On upload
-              [](AsyncWebServerRequest *request, unsigned char* data, unsigned int len, unsigned int index, unsigned int total) { // On body
+              [](AsyncWebServerRequest *request){},
+              NULL,
+              [](AsyncWebServerRequest *request, unsigned char* data,
+                 unsigned int len, unsigned int index, unsigned int total) {
                 char body[len+1];
                 for (int i = 0; i < len; i++)
                 {
@@ -189,6 +190,7 @@ void setup()
                 request->send(200, "text/plain", String(Setpoint));
               });
     Serial.println("... OK");
+
     Serial.print("Starting web server");
     server.begin();
     Serial.println("... OK");
@@ -196,14 +198,17 @@ void setup()
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
     Input = GET_TEMP(TEMP1_PIN);
     myPID.Compute();
+
+    // Only save data every SAMPLE_TIME milliseconds
     if (millis() - sampleTimer > SAMPLE_TIME)
     {
         sampleTimer = millis();
         temperatureBuf.put(Input);
     }
+    // Default PID output 0-255 (default Arduino PWM range). Only turn on relay
+    // if the output is above half
     if (Output > 128)
     {
         digitalWrite(RELAY1_PIN, HIGH);
