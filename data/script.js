7a9f0ffcde07de47ccbc281e8d14120dc5ff0a70
Felix <felix@malmsjo.com>
2022-03-23 00:22:05 +0100

    Add styling and some ux for the number input

--- data/script.js
+++ data/script.js
@@ -17,21 +17,39 @@ window.onload = () => {
         tempRange.value = text;
     });
     const updateGraph = () => {
+        const maxTemp = 120;
+        graphWidth = graph.viewBox.animVal.width;
+        // TODO: offset graph and add degrees
+        graphHeight = graph.viewBox.animVal.height - 15;
         console.log();
         fetch("/api/v1/get_timestep").then(response => response.text()).then(timestep => {
             fetch("/api/v1/get_data").then(response => response.text()).then(text => {
                 console.log(timestep);
                 const array = text.split(" ");
+                const hlines = 11;
+                const vlines = Math.max(9, (array.length-2));
                 const data = array.map((entry, i) => ({
-                    time: (graph.viewBox.animVal.width * i / array.length),
-                    temp: (graph.viewBox.animVal.height * (1 - entry / 125))
+                    time: (graphWidth * i / (array.length-1)),
+                    temp: (graphHeight * (1 - entry / maxTemp))
                 }));
                 console.log(data);
                 graph.innerHTML = `
+                ${[...Array(hlines).keys()].map(
+                    h => `<line x1="0" y1="${(h+1)*graphHeight/(hlines+1)}"
+                           x2="${graphWidth}"
+                           y2="${(h+1)*graphHeight/(hlines+1)}"/>`).join("\n")}
+                ${[...Array(vlines).keys()].map(
+                    v => {
+                        const x = (v+1)*graphWidth/(vlines+1);
+                        const bufferTime = array.length * timestep;
+                        return `<line x1="${x}" y1="0" 
+                        x2="${x}"
+                        y2="${graphHeight}"/>
+                        <text x="${x}" y="${graphHeight + 14}">T-${Math.round(bufferTime / (v+1) / 10) / 100}</text>`
+                    }).join("\n")}
                 <polyline
                    fill="none"
-                   stroke="#0074d9"
-                   stroke-width="3"
+                   stroke-width="1"
                    points="${data.map(element => `${element.time},${element.temp}`).join(",")}"/>`;
             });
         });
