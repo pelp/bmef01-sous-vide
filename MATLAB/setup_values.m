89c0c8712b053b34217339c3c402f21b89830e3b
Felix <felix@malmsjo.com>
2022-03-25 14:01:45 +0100

    Add matlab simulation of sous vide system

---
+++ MATLAB/setup_values.m
@@ -0,0 +1,17 @@
+Kp = 10
+Ki = 10
+
+P = 300 % Power output of the heating element
+
+c = 4180 % Water heatcapacity
+m = 2 % kg of water
+
+A = 0.07 % Approx area of 30cm diameter circle
+a = 50 % Approx Air heat transfer koef (10 - 100)
+
+Ta = 19 % Ambient temperature
+
+Ts = 1 % Sample time
+Td = 0.5 % Temperature resolution
+
+Sp = 82 % Temperature setpoint
